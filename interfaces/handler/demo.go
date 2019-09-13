1b31cf1db47d36c3e4e568b509112de058dbabdd
Miyajima Kenta <shadowlink0122@gmail.com>
2019-09-14 02:23:49 +0900

    handler

---
+++ interfaces/handler/demo.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/shadowlink0122/CASystem/interfaces/di"
+)
+
+type demoGetResponse struct {
+	Message string `json:"message"`
+}
+
+// HandleDemoGet is GET Demo message
+func HandleDemoGet() http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		// 
+		msg, err := di.Demo.GetMessage()
+		if err != nil {
+			log.Println(err)
+			response.InternalServerError(writer, "Internal Server Error")
+			return
+		}
+
+		// response
+		response.Success(
+			writer,
+			demoGetResponse{
+				Message: msg
+			}
+		)
+	}
+}
